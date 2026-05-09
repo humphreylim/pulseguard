5eae9a1bb3ab3b680f0222c6e289bac7296b1211
humphreylim <9331894+humphreylim@users.noreply.github.com>
2026-05-09 19:26:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'pulseguard-v1';
+const CACHE = 'pulseguard-v4-crimson';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
